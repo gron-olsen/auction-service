3cc338f988da617a1deaace3b2450f7cb1c84771
MagnusHeesch <magnusheesch@live.dk>
2024-05-23 16:58:31 +0200

    Tests committed

--- auction-serviceAPI.Test/UnitTest1.cs
+++ auction-serviceAPI.Test/UnitTest1.cs
@@ -1,15 +1,55 @@
+using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using auctionServiceAPI.Controllers;
+using auctionServiceAPI.Services;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Linq;
+global using NUnit.Framework;
+using auctionServiceAPI.Models;
 namespace auction_serviceAPI.Test;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+    private readonly AuctionController _auctionController;
+    private readonly Mock<ILogger<AuctionController>> _loggerMock;
+    private readonly Mock<AuctionService> _auctionServiceMock;
+    private readonly Mock<IConfiguration> _configurationMock;
 
-    [Test]
-    public void Test1()
+    public AuctionControllerTests()
     {
-        Assert.Pass();
+        _loggerMock = new Mock<ILogger<AuctionController>>();
+        _auctionServiceMock = new Mock<AuctionService>();
+        _configurationMock = new Mock<IConfiguration>();
+
+        _configurationMock.SetupGet(x => x["server"]).Returns("localhost");
+        _configurationMock.SetupGet(x => x["port"]).Returns("27017");
+        _configurationMock.SetupGet(x => x["collection"]).Returns("auctionCol");
+        _configurationMock.SetupGet(x => x["database"]).Returns("auctionDB");
+
+        _auctionController = new AuctionController(_loggerMock.Object, _configurationMock.Object);
+
+        [Test]
+        public void PostAuctionOK_Test()
+        {
+            //arrange
+
+            //act
+
+            //assert
+            Assert.Pass();
+
+        }
+        [Test]
+        public void PostAuctionBid_Test()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
     }
 }
