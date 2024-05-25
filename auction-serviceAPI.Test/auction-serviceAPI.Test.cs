3129596da07caf1e02d05fcbc6e22022b8ba3ad3
MagnusHeesch <magnusheesch@live.dk>
2024-05-25 16:47:29 +0200

    tests again

--- auction-serviceAPI.Test/auction-serviceAPI.Test.cs
+++ auction-serviceAPI.Test/auction-serviceAPI.Test.cs
@@ -1,40 +1,45 @@
-using Moq;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using auctionServiceAPI.Controllers;
 using auctionServiceAPI.Services;
 using auctionServiceAPI.Models;
 using Microsoft.Extensions.Configuration;
 using System.Net;
-using System.Linq;
 global using NUnit.Framework;
 
 
-namespace auction_serviceAPI.Test;
-
-
-public class AuctionControllerTests
+namespace auctionServiceAPI.Test
 {
-    private readonly AuctionController _auctionController;
-    private readonly Mock<ILogger<AuctionController>> _loggerMock;
-    private readonly Mock<AuctionService> _auctionServiceMock;
-    private readonly Mock<IConfiguration> _configurationMock;
-    [SetUp]
-    public AuctionControllerTests()
+    [TestClass]
+    public class AuctionControllerTests
     {
-        _loggerMock = new Mock<ILogger<AuctionController>>();
-        _auctionServiceMock = new Mock<AuctionService>();
-        _configurationMock = new Mock<IConfiguration>();
+        private AuctionController _auctionController;
+        private static Mock<ILogger<AuctionController>> _loggerMock;
+        private Mock<AuctionService> _auctionServiceMock;
+        private Mock<IConfiguration> _configurationMock;
 
-        _configurationMock.SetupGet(x => x["server"]).Returns("localhost");
-        _configurationMock.SetupGet(x => x["port"]).Returns("27017");
-        _configurationMock.SetupGet(x => x["collection"]).Returns("auctionCol");
-        _configurationMock.SetupGet(x => x["database"]).Returns("auctionDB");
 
-        _auctionController = new AuctionController(_loggerMock.Object, _configurationMock.Object);
+        public AuctionControllerTests()
+        {
+            _loggerMock = new Mock<ILogger<AuctionController>>();
+            _auctionServiceMock = new Mock<AuctionService>();
+            _configurationMock = new Mock<IConfiguration>();
 
-        [Test]
-        public async Task PostAuction_Test()
+            _configurationMock.SetupGet(x => x["server"]).Returns("localhost");
+            _configurationMock.SetupGet(x => x["port"]).Returns("27017");
+            _configurationMock.SetupGet(x => x["collection"]).Returns("auctionCol");
+            _configurationMock.SetupGet(x => x["database"]).Returns("auctionDB");
+
+            _auctionController = new AuctionController(_loggerMock.Object, _configurationMock.Object);
+        }
+        [TestMethod]
+        public void PostAuction_Test()
         {
             //Arrange
             var auctionsproducts = new AuctionProduct[]
@@ -52,8 +57,8 @@ public class AuctionControllerTests
         }
 
 
-        [Test]
-        public async void GetAuctionPrice_Test()
+        [TestMethod]
+        public void GetAuctionPrice_Test()
         {
             //Arrange
             var auctionsproducts = new AuctionProduct[]
